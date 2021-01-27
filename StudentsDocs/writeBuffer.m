16ab7fd0f2c7a491b61b1ad22d39468a11eb5965
Jehxon <richard.arnaud@laposte.net>
2021-01-27 15:03:12 +0100

    merged buffer work from iker manually

--- StudentsDocs/writeBuffer.m
+++ StudentsDocs/writeBuffer.m
@@ -2,30 +2,43 @@ function writeBuffer(dataElem)
 
 global head 
 global tail 
-global y 
+global buffer 
 global isFull
 global isEmpty
 global rango
 
 isEmpty = 0;
 
+% Magnet?
+
+m = dataElem(15:16,:);
+if(m(1) == 0 && m(2) == 0)
+    isMagnet = 0;
+else
+    isMagnet = 1;
+end
+
+
 if(isFull == 1)
     disp('The buffe is Full');
 else
     
 % write data into the buffer
-    y(head) = dataElem;
-    head=head+1;
+    buffer(:,head) = dataElem;
     
-% If we arrive to the end -> reset index
-    if(head == rango + 1)
-        head = 1;
-    end  
+% if there is not a magnet, we do not increase head
+    if(isMagnet == 1)
+        head=head+1;
     
-% Full? 
-    if(head == tail)
-        isFull = 1;
+    % If we arrive to the end -> reset index
+        if(head == rango + 1)
+            head = 1;
+        end  
+
+    % Full? 
+        if(head == tail)
+            isFull = 1;
+        end
     end
-    
 end
 end
